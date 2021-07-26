f64e00a863266c361d7d13ce4a545ea2ccd5100e
jiki <452956364@qq.com>
2021-07-26 21:46:30 +0800

    fix

--- src/components/order_table/index.tsx
+++ src/components/order_table/index.tsx
@@ -1,10 +1,15 @@
 import React, { Component, ReactNode } from 'react'
 import { Table } from 'antd'
-
+import axios from 'axios'
 
 class OrderTable extends Component {
   constructor (props:any) {
     super(props)
+  }
+  componentDidMount () {
+    axios.get('/api/test').then(res => {
+      console.log(res)
+    })
   }
   render ():ReactNode {
     return (
